b00087fd27311a24ee6b59cffb2828b5e57ba06c
THartigan <57837418+THartigan@users.noreply.github.com>
2024-11-10 18:08:39 +0000

    added gravity vector export

---
+++ ActiveAlarm/FileController.swift
@@ -0,0 +1,8 @@
+//
+//  FileController.swift
+//  ActiveAlarm
+//
+//  Created by Thomas Hartigan on 09/07/2024.
+//
+
+import Foundation
---
+++ ActiveAlarm/FileDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FileDetailView.swift
+//  ActiveAlarm
+//
+//  Created by Thomas Hartigan on 08/07/2024.
+//
+
+import SwiftUI
+
+struct FileDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FileDetailView()
+}
